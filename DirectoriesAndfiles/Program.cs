fdbcf850ba5554138a4dc7afc1c4cbc637be8f0d
unknown <mammutipoja1@gmail.com>
2020-11-19 17:01:51 +0200

    DirectoriesAndFiles

---
+++ DirectoriesAndfiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\mammu\samples";
+            string[] allDirectories = Directory.GetDirectories(rootPath);
+            //ReadArray(allDirectories);
+
+            string[] includeNestedDirectories = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            //ReadArray(includeNestedDirectories);
+            string[] files = Directory.GetFiles(rootPath, "*.*",SearchOption.AllDirectories);
+            ReadArray(files);
+
+
+
+        }
+
+        public static void ReadArray(string[] array)
+        {
+            foreach(string line in array)
+            {
+              Console.WriteLine(line);
+
+            }
+            
+        }
+    }
+}
---
+++ WriteFilesAndDirectories/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace WriteFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteFilePaths();
+
+
+        }
+
+        public static void WriteFilePaths()
+        {
+            string filepath = @"C:\Users\mammu\samples\files.txt";
+            string directoryPath = @"C:\Users\mammu\samples";
+
+            string[] files = Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories);
+
+            File.WriteAllLines(filepath, files);
+        }
+    }
+}
